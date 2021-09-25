aa3448ec379d32e5e497044ee66442c97828fb5c
Technology <technology@moonandstars.ch>
2021-09-25 16:31:56 +0200

    fix json response

--- get-story-lambda/index.js
+++ get-story-lambda/index.js
@@ -17,8 +17,26 @@ const getStoryById = async (storyId) => {
 
   client.release();
 
-  console.log("found story:", response);
-  return response.rows;
+  if (!response.rows[0]) {
+    return null;
+  }
+
+  console.log("found story:", response.rows[0].id);
+
+  const foundStory = {
+    id: response.rows[0].id,
+    title: response.rows[0].title,
+    lead: response.rows[0].lead,
+    created_at: response.rows[0].created_at,
+    updated_at: response.rows[0].updated_at,
+    thumbnail: response.rows[0].thumbnail,
+    body: JSON.parse(response.rows[0].body),
+    tags: response.rows[0].tags.split(","),
+    consume_time: response.rows[0].consume_time,
+    rating: response.rows[0].rating,
+  };
+
+  return foundStory;
 };
 
 exports.handler = async (event, context) => {
@@ -27,14 +45,23 @@ exports.handler = async (event, context) => {
   console.log("called get story endpoint");
   console.log("story id received:", story);
 
-  const fullStory = await getStoryById(story);
+  const foundStory = await getStoryById(story);
+
+  if (!foundStory) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "not found",
+      }),
+    };
+  }
 
   var response = {
     statusCode: 200,
     body: JSON.stringify({
       message: "success",
       storyId: story,
-      story: fullStory,
+      story: foundStory,
     }),
   };
   return response;
